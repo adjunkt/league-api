9dbba3cc1359d1dea6bc1dc503aa3e565e90cbca
ksure <kyle.scherling@gmail.com>
2017-07-29 11:22:16 -0700

    Update user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,20 +1,11 @@
 FactoryGirl.define do
   factory :user do
-    first_name 'John'
-    last_name 'Doe'
-    password 'P@ssword123'
+    first_name 'Gordie'
+    last_name 'Howe'
+    password 'Passw0rd!'
     sequence(:email) do |n|
       "#{first_name}-#{last_name}-#{n}@example.com".downcase
     end
     confirmed_at { Time.now }
-
-    trait :admin do
-      admin true
-    end
-
-    factory :admin_user, traits: [:admin]
-    factory :unconfirmed_user do
-      confirmed_at nil
-    end
   end
 end
