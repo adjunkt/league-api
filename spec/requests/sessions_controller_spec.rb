d909609dba9120785eca16dc709b4ff4f5e7bf40
ksure <kyle.scherling@gmail.com>
2017-07-29 14:28:15 -0700

    Adds session controller and stubs specs

---
+++ spec/requests/sessions_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :controller do
+  describe '#create' do
+    it 'is ok' do
+      expect(true).to be true
+    end
+  end
+
+  describe '#destroy' do
+  end
+end
