a521004d05fd5f314877201eade487a4bd619c8f
ksure <kyle.scherling@gmail.com>
2017-08-01 22:22:03 -0700

    Adds user controller and route for users/me

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::API
   include JSONAPI::ActsAsResourceController
 
+  before_action :doorkeeper_authorize!
+
   def context
     { current_user: current_user }
+  end
+
+  def current_user
+    @current_user ||= User.find_by(
+      id: doorkeeper_token.try(:resource_owner_id))
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   use_doorkeeper do
     controllers tokens: 'tokens'
   end
-  # devise_for :users
+
+  get '/users/me', to: 'users#me', constraints: { id: 'me' }
+  jsonapi_resources :users
 end
