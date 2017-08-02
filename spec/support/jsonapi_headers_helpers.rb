ebab8b1cd628175e30bc1a135e360f77285a7bec
ksure <kyle.scherling@gmail.com>
2017-08-01 22:40:25 -0700

    Adds jsonapi headers spec helper

---
+++ spec/support/jsonapi_headers_helpers.rb
@@ -0,0 +1,15 @@
+module JSONAPIHeadersHelpers
+  def jsonapi_media_type_headers
+    {
+      'ACCEPT' => JSONAPI_MEDIA_TYPE,
+      'CONTENT_TYPE' => JSONAPI_MEDIA_TYPE
+    }
+  end
+
+  def headers_for_user(user)
+    access_token = create(:access_token, resource_owner_id: user.id)
+    jsonapi_media_type_headers.merge(
+      'Authorization' => "Bearer #{access_token.token}"
+    )
+  end
+end
