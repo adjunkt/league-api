cb596057e13579c7b1934614627a368f63b1c059
Jonathan So <jso@focusvision.com>
2017-08-13 17:48:05 -0700

    Remove unneeded line

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
   it 'creates an organization' do
-    create(:organization, name: 'My Organization')
-
     expect(build(:organization, name: 'My Organization')).to be_valid
   end
 end
