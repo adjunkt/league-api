f69fb8eb91508501c50dc0441f7a5b957ac1c8ed
Jonathan So <jso@focusvision.com>
2017-08-13 17:45:35 -0700

    Organization name

--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 20170813234755) do
   end
 
   create_table "organizations", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
