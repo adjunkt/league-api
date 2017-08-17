ac876e652145a37d0848396adffe504efb4e284d
ksure <kyle.scherling@gmail.com>
2017-08-16 23:57:41 -0700

    Adds model attrs and tests for league

--- spec/factories/leagues.rb
+++ spec/factories/leagues.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :league do
-    
+    name 'Gold'
   end
 end
--- spec/models/league_spec.rb
+++ spec/models/league_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe League, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'creates a league' do
+    organization = create(:organization)
+    sport = create(:sport)
+
+    expect(
+      build(
+        :league,
+        name: 'Peewee',
+        organization: organization,
+        sport: sport
+      )
+    ).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    organization = create(:organization)
+    sport = create(:sport)
+
+    expect(
+      build(
+        :league,
+        name: nil,
+        organization: organization,
+        sport: sport
+      )
+    ).to_not be_valid
+  end
 end
